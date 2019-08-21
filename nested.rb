2265b309cd4845505cd0f93a408d8453949f1e6d
Michael Lynch <michael@michaelrlynch.com>
2019-08-21 02:38:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,8 @@ def adding_matz
   }
 =end
     programmer_hash[:yukihiro_matsumoto] = {}
-    programmer_hash[:yukihiro_matsumoto][:known_for] << 
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 	programmer_hash = 
  		{
         :grace_hopper => {
