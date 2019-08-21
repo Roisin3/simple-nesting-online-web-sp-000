6870e0b1708ace5b361eafd4c6f80ef28f7bd405
Michael Lynch <michael@michaelrlynch.com>
2019-08-21 02:12:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay]
+  programmer_hash[:alan_kay][:languages] << "Object Orientation"
      
 end
 
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchies] << "Object Orientation"
+  programmer_hash[:dennis_ritchie]
 end
 
 def adding_matz
